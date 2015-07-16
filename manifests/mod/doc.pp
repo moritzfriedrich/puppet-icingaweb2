b57113d9da2d93adba5200699ccf2ffeff09091d
Moritz Friedrich <mf@cyon.ch>
2015-07-16 13:24:05 +0200

    fixed enabled options

--- manifests/mod/doc.pp
+++ manifests/mod/doc.pp
@@ -7,15 +7,18 @@ class icingaweb2::mod::doc (
 
     validate_bool($enabled)
 
+    $doc_ensure = $enabled ? {
+        false => "absent",
+        true  => "link",
+    }
+
     file { "${::icingaweb2::config_dir}/modules/doc":
         ensure => link,
         target => "${::icingaweb2::web_root}/modules/doc"
     }
 
-    if $enabled {
-        file { "${::icingaweb2::config_dir}/enabledModules/doc":
-            ensure => link,
-            target => "${::icingaweb2::web_root}/modules/doc"
-        }
+    file { "${::icingaweb2::config_dir}/enabledModules/doc":
+        ensure => $doc_ensure,
+        target => "${::icingaweb2::web_root}/modules/doc"
     }
 }
